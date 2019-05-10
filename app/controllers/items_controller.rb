941ec4923b5e94229b3cf6ab293ca6503f92f6ae
Your Name <you@example.com>
2019-05-10 21:31:38 +0300

    add pagination

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,6 +9,7 @@ class ItemsController < ApplicationController
     @items = @items.search_color(params[:search_color]) if params[:search_color].present?
     @items = @items.search_color(params[:search_brand]) if params[:search_brand].present?
     @items = @items.search_category(params[:search_category]) if params[:search_category].present?
+    #@items = @items.paginate(page: 1, per_page: 20)
     respond_to do |format|
       format.html
       format.xml { render :xml => @people.to_xml }
