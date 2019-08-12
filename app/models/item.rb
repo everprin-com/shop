f4bb743f45b80aa3dc4bf8022652628d88879a1b
Your Name <you@example.com>
2019-08-12 07:09:31 +0300

    fix herders

--- app/models/item.rb
+++ app/models/item.rb
@@ -32,17 +32,16 @@ class Item < ActiveRecord::Base
 
   def self.create_header
     Header.delete_all
-    wooman_catalogues = Item.where('sex && ARRAY[?]::varchar[]', "wooman").select(:category).uniq.map(&:category)
-
+    wooman_catalogues = Item.where('sex && ARRAY[?]::varchar[]', "wooman").select(:category).uniq.map(&:category).compact
     wooman_catalogues.map do |catalogue|
       count = Item.where(category: catalogue).count
-      group = GROUP.select{ |key, hash| hash.include?(catalogue.capitalize) }.keys[0].to_s
+      group = GROUP.select{ |key, hash| hash.include?(catalogue&.capitalize) }.keys[0].to_s
       Header.create!(count_items: count, catalogue: catalogue, group: group, male: false)
     end
-    man_catalogues = Item.where('sex && ARRAY[?]::varchar[]', "man").select(:category).uniq.map(&:category)
+    man_catalogues = Item.where('sex && ARRAY[?]::varchar[]', "man").select(:category).uniq.map(&:category).compact
     man_catalogues.map do |catalogue|
       count = Item.where(category: catalogue).count
-      group = GROUP.select{ |key, hash| hash.include?(catalogue.capitalize) }.keys[0].to_s
+      group = GROUP.select{ |key, hash| hash.include?(catalogue&.capitalize) }.keys[0].to_s
       Header.create!(count_items: count, catalogue: catalogue, group: group, male: true)
     end
   end
