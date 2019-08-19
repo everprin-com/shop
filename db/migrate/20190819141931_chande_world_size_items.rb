0e10d813e983b4f11813ad8773c1cca55183bf42
Your Name <you@example.com>
2019-08-20 00:12:46 +0300

    fix migration

--- db/migrate/20190819141931_chande_world_size_items.rb
+++ db/migrate/20190819141931_chande_world_size_items.rb
@@ -1,5 +1,5 @@
 class ChandeWorldSizeItems < ActiveRecord::Migration
   def change
-    change_column :items, :size_world, :json
+    change_column :items, :size_world, :json, 'json USING CAST(size_world AS json)'
   end
 end
