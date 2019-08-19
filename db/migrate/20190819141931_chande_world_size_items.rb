a9a9fd5f099afd86d59866004d278562ed3a221a
Your Name <you@example.com>
2019-08-20 00:10:14 +0300

    fix

--- db/migrate/20190819141931_chande_world_size_items.rb
+++ db/migrate/20190819141931_chande_world_size_items.rb
@@ -1,12 +1,5 @@
 class ChandeWorldSizeItems < ActiveRecord::Migration
-  def self.up
-    change_table :items do |t|
-      t.change :items, :size_world, :json
-    end
-  end
-  def self.down
-    change_table :items do |t|
-      t.change :items, :size_world, :string
-    end
+  def change
+    change_column :items, :size_world, :json
   end
 end
