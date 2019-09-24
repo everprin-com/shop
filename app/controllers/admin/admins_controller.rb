b2e5a3b10f24d9867e4ffc531ba75ab2c561d997
Your Name <you@example.com>
2019-09-25 00:31:42 +0300

    fix convertor

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -11,7 +11,27 @@ module Admin
     end
 
     def convert_xls
-      %x[bundle exec rake parser_excel:parser_excel]
+      files = Dir.entries("public/excel/parser")
+      files.delete(".")
+      files.delete("..")
+      files.map do |file_name|
+        #file = Roo::Spreadsheet.open("public/excel/parser/#{file_name}")
+        file = File.open("public/excel/parser/#{file_name}", "r")
+        drop_ship_name = file_name.split("_")[0]
+        artwork = ActionDispatch::Http::UploadedFile.new(
+          filename: file_name,
+          content_type: "application/vnd.ms-excel",
+          tempfile: file,
+        )
+        @items_import = ItemsImport.new({:file => artwork}, drop_ship_name)
+        if @items_import.save
+          Item.update_size_same_items
+          Item.delete_bad_products
+          Item.create_header
+          FilterOption.delete_all
+          FilterOption.create!(Item.generate_filters(Item.all))
+        end
+      end
       redirect_to "/admin/admins"
     end
 
