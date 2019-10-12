df45a6ca6251651ece1fc29f358cd47f58e04c0d
Your olegsta <olegsta@helpsyst.com>
2019-10-12 13:04:42 +0300

    add category

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,12 +1,20 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :price, :article, :name, :description, :color, :picture, :brand, :sex, :small_picture,
   :season, :male, :size, :country, :category, :available_product, :size_world, :composition, :drop_ship,
-  :created_at, :slug_id, :category_translate
+  :created_at, :slug_id, :category_translate, :group
 
   root false
 
   def size_world
     object.size_world&.to_s
+  end
+
+  def group
+    searched_key = ""
+    Item::GROUP.map do |key, value|
+      searched_key = key.to_s if value.include?(object.category)
+    end
+    searched_key
   end
 
   def size
