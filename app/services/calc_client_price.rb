7cb3993212746c7d5349fb860a9234718d46940a
Your Name <you@example.com>
2019-10-26 21:53:00 +0300

    fi cals

--- app/services/calc_client_price.rb
+++ app/services/calc_client_price.rb
@@ -4,7 +4,7 @@ class CalcClientPrice
 
   def self.calc_client_price(price, name_drop_ship={})
     return unless price
-    if name_drop_ship.capitalize == "Favoritti"
+    if name_drop_ship&.capitalize == "Favoritti"
       (price.to_f * INCOME_DEPEND_DROP_SHIP["Favoritti"]).ceil
     elsif price.to_f * 0.3 > 80
       (price.to_f * 1.3).ceil
