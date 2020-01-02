7d45e47322e608d942d371b65024f9bf7ecbeade
Your olegsta <olegsta@helpsyst.com>
2020-01-02 11:04:09 +0200

    add voted

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,7 +1,9 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :price, :article, :name, :description, :color, :picture, :brand, :sex, :small_picture,
   :season, :male, :size, :country, :category, :available_product, :size_world, :composition, :drop_ship,
-  :created_at, :slug_id, :category_translate, :group
+  :created_at, :slug_id, :category_translate, :product_comment_counts
+
+  has_one :average_voted
 
   root false
 
@@ -9,16 +11,20 @@ class ItemSerializer < ActiveModel::Serializer
     object.size_world&.to_s
   end
 
-  def group
-    searched_key = ""
-    Item::GROUP.map do |key, value|
-      searched_key = key.to_s if value.include?(object.category)
-    end
-    searched_key
-  end
+  # def group
+  #   searched_key = ""
+  #   Item::GROUP.map do |key, value|
+  #     searched_key = key.to_s if value.include?(object.category)
+  #   end
+  #   searched_key
+  # end
 
   def size
     object.size&.sort
+  end
+
+  def product_comment_counts
+    object.product_comments.length
   end
 
   def name
