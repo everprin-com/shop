442a7ef6ee53d76f767bcccfab9d3a07b9783aee
Your olegsta <olegsta@helpsyst.com>
2020-04-19 14:00:47 +0300

    fix

--- app/models/admins/admin.rb
+++ app/models/admins/admin.rb
@@ -3,10 +3,10 @@ module Admins
   class Admin < ActiveRecord::Base
 
       XLS_LINK = [
-        #"http://timeofstyle.com/download/tos_actual_price_and_stock.xls",
+        "http://timeofstyle.com/download/tos_actual_price_and_stock.xls",
         "https://ager.ua/download/ager_actual_price_and_stock.xls",
-        #"https://garne.com.ua/files/prices_clothes.xlsx",
-        #"https://favoritti.com/upload/favoritti_com_export_csv_opt.xlsx",
+        "https://garne.com.ua/files/prices_clothes.xlsx",
+        "https://favoritti.com/upload/favoritti_com_export_csv_opt.xlsx",
       ]
 
       XML_LINK = [
